c6c8971d54e63e7113464d702e20c93942b54536
IrepY <malnaspeter05@gmail.com>
2025-05-01 23:31:55 +0200

    Small cleanup and removed old debug logs

--- task-manager-frontend/src/i18n.js
+++ task-manager-frontend/src/i18n.js
@@ -1,12 +1,10 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
+import LanguageDetector from 'i18next-browser-languagedetector'
 
-// Import translation files
-import translationEN from './locales/en/translation.json';
-import translationHU from './locales/hu/translation.json';
+import translationEN from './locales/en/translation.json'
+import translationHU from './locales/hu/translation.json'
 
-// The translations
 const resources = {
   en: {
     translation: translationEN
@@ -14,26 +12,22 @@ const resources = {
   hu: {
     translation: translationHU
   }
-};
+}
 
 i18n
-  // Detect user language
   .use(LanguageDetector)
-  // Pass the i18n instance to react-i18next
   .use(initReactI18next)
-  // Initialize i18next
   .init({
     resources,
-    fallbackLng: 'en', // Use English if user language not available
-    debug: process.env.NODE_ENV === 'development',
+    fallbackLng: 'en', 
     interpolation: {
-      escapeValue: false // React already secures from XSS
+      escapeValue: false
     },
     detection: {
       order: ['localStorage', 'navigator'],
       lookupLocalStorage: 'language',
       caches: ['localStorage']
     }
-  });
+  })
 
-export default i18n;
+export default i18n

219e8068386ff697bd7853d7f777e18424d59865
IrepY <malnaspeter05@gmail.com>
2025-05-03 17:50:34 +0200

    Bare minimum for profile placeholder tab

--- task-manager-frontend/src/components/ProfileView.jsx
+++ task-manager-frontend/src/components/ProfileView.jsx
@@ -1,25 +1,17 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import { useTranslation } from 'react-i18next'
 
-function ProfileView({ isDesktop }) {
+function AboutView({ isDesktop }) {
   const { t } = useTranslation()
+  
   return (
-    <div className={`p-4 sm:p-6 h-full flex flex-col ${!isDesktop ? 'pb-20' : ''} ${isDesktop ? 'pl-2 sm:pl-4' : ''}`}>
-      {isDesktop && (
-        <div className="mb-6 flex-shrink-0">
-          <h2 className="text-2xl font-semibold text-gray-700 dark:text-gray-200 text-left">{t('menu.profile')}</h2>
-        </div>
-      )}
-      <div className="flex-grow text-gray-500 dark:text-gray-400">
-        <p>{t('profile.placeholderText')}</p>
-      </div>
+    <div>
+       {isDesktop && (
+          <h2 className="mb-4 text-2xl font-semibold text-gray-700 dark:text-gray-200">{t("menu.profile")}</h2>
+       )}
+        <p className="text-l text-gray-500 dark:text-gray-400">{t("profile.placeholderText")}</p>
     </div>
   )
 }
 
-ProfileView.propTypes = {
-  isDesktop: PropTypes.bool.isRequired,
-}
-
-export default ProfileView
+export default AboutView
